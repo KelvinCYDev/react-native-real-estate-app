76efb6d4cad6488ed05e01081b6163e1dcd43415
KelvinCYDev <kelvincy.dev@gmail.com>
2025-02-01 17:16:40 -0800

    Add: NativeWind setup

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="font-bold my-10">Welcome to ReState</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
